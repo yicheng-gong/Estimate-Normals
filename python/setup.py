7fec6e3be94323e82f57283c59b70862059a807e
yicheng <yicheng-gong@outlook.com>
2023-04-15 17:50:38 +0100

    modify

---
+++ python/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy
+
+
+ext_modules = [Extension(
+       "NormalEstimatorHoughCNN",
+       sources=["NormalEstimatorHoughCNN.pyx", "houghCNN.cxx"],
+       include_dirs=["../third_party_includes/", numpy.get_include()],
+       language="c++",             # generate C++ code
+       extra_compile_args = ["-fopenmp", "-std=c++11"],
+       extra_link_args=['-lgomp']
+  )]
+
+setup(
+    name = "Hough Normal Estimator CNN",
+    ext_modules = ext_modules,
+    cmdclass = {'build_ext': build_ext},
+)
